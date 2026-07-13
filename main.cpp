1998cf30d877f8a0eebf8dbf57cfa31ce99a0354
Monish H. Giani <256585194+thelazybastard@users.noreply.github.com>
2026-07-14 01:10:23 +0700

    finished l and c features
    tool detects line and byte/character counts

--- main.cpp
+++ main.cpp
@@ -1,4 +1,63 @@
+#include <cstdlib>
+#include <fstream>
 #include <iostream>
 #include <string>
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) { return 0; }
+bool checkForWord(std::string_view word, std::string_view subtext);
+void hasFile(std::string filename, char command);
+void hasNoFile(char command);
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
+    if (argc < 2 || argc > 3)
+        return 1;
+
+    // check if user passed in file or not in case they want to check word count
+    // with external tools like cat before passing in text to this tool
+    checkForWord(argv[1], ".txt") ? hasFile(argv[2], argv[1][1])
+                                  : hasNoFile(argv[1][1]);
+
+    return 0;
+}
+
+bool checkForWord([[maybe_unused]] std::string_view text,
+                  [[maybe_unused]] std::string_view subtext) {
+
+    // for (int i = 0, len = text.size(); i < len; i++) {
+
+    // }
+    return true;
+}
+
+void hasFile(std::string filename, char command) {
+    std::ifstream file(filename);
+    if (!file.is_open())
+        std::exit(1);
+
+    int tracker{};
+
+    std::string line{};
+    char character{};
+
+    switch (command) {
+    case 'c':
+        while (file.get(character)) {
+            ++tracker;
+        }
+        break;
+    case 'l':
+        while (std::getline(file, line)) {
+            ++tracker;
+        }
+        break;
+    case 'w':
+        break;
+    case 'm':
+        break;
+    default:
+        break;
+    }
+
+    std::cout << tracker << " " << filename << "\n";
+}
+
+void hasNoFile(char command) { std::cout << command; }
